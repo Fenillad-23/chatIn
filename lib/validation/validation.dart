e9f8ec90a1fed7804de6e098c0f6a17b2cdc072c
vaidehikheni <96909237+vaidehikheni@users.noreply.github.com>
2022-01-01 12:19:33 +0530

    changes in validation

--- lib/validation/validation.dart
+++ lib/validation/validation.dart
@@ -2,47 +2,37 @@ class Validators {
   static String? userNameValidator(String value, String type, int length) {
     String namePtn = r'(^[a-zA-Z0-9]{6,15}$)';
     RegExp regExp = new RegExp(namePtn);
-    if (value.length == 0) {
+    if (value.isEmpty) {
       return "$type is required";
     } else if (value.length > length) {
       return "$type less then $length character";
-    } else if (!regExp.hasMatch(namePtn)) {
+    } else if (!regExp.hasMatch(value)) {
       return "$type is invalid";
     }
     return null;
   }
 
   static String? passwordValidator(String value, String type, int length) {
-    String passPtn = r'(^[a-zA-Z0-9]{6,15}$)';
-    RegExp regExp = new RegExp(passPtn);
-    if (value.length == 0) {
+    String passPtn = r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$';
+    RegExp regExp =  RegExp(passPtn);
+    if (value.isEmpty) {
       return "$type is required";
     } else if (value.length > length) {
       return "$type less then $length character";
-    } else if (!regExp.hasMatch(passPtn)) {
-      return "$type is invalid";
-    }
-    return null;
-  }
-
-  static String? emailValidator(String value, String type, int length) {
-    String emailPtn = r'(^[a-zA-Z0-9]{6,15}$)';
-    RegExp regExp = new RegExp(emailPtn);
-    if (value.length == 0) {
-      return "$type is required";
-    } else if (value.length > length) {
-      return "$type less then $length character";
-    } else if (!regExp.hasMatch(emailPtn)) {
-      return "$type is invalid";
+    } else if (!regExp.hasMatch(value)) {
+      return "Invalid - please follow this formate UserName@47";
     }
     return null;
   }
 
   static String? ContactNumberValidator(String value, String type) {
+    int v = value.length;
     if (value.isEmpty) {
       return "$type can't be null";
-    } else if (value.length > 10) {
+    } else if (v > 10) {
       return "$type can't be more than 10 ";
+    } else if (v < 10){
+      return "$type can't be less than 10";
     }
     return null;
   }
