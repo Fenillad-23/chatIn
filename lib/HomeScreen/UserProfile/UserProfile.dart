cb7b6c4a376b0c01deab8e3f6ec1f558958d1669
Fenillad-23 <75263786+Fenillad-23@users.noreply.github.com>
2022-01-24 20:27:22 +0530

    done

--- lib/HomeScreen/UserProfile/UserProfile.dart
+++ lib/HomeScreen/UserProfile/UserProfile.dart
@@ -29,54 +29,46 @@ class _UserProfileState extends State<UserProfile> {
         child: ListView(
           children: [
             Container(
-              height: 165,
+              height: 155,
               width: MediaQuery.of(context).size.width,
-              child: Card(
-                elevation: 8,
-                shape: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: BorderSide(color: Colors.white),
-                ),
-                shadowColor: Colors.blue,
-                child: Row(
-                  children: <Widget>[
-                    Column(
-                      children: <Widget>[
-                        Padding(
-                          padding: const EdgeInsets.all(10.0),
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(100),
-                            child: Image.asset(
-                              'assets/images/user.jpeg',
-                              width: 125,
-                              height: 125,
-                              fit: BoxFit.contain,
-                            ),
+              child: Row(
+                children: <Widget>[
+                  Column(
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(100),
+                          child: Image.asset(
+                            'assets/images/user.jpeg',
+                            width: 125,
+                            height: 125,
+                            fit: BoxFit.contain,
                           ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Column(
+                      children: <Widget>[
+                        Text(
+                          "full name",
+                          style: TextStyle(fontSize: 18, letterSpacing: 0.4),
+                        ),
+                        Text(
+                          "Bio",
+                          style: TextStyle(fontSize: 16, letterSpacing: 0.4),
                         ),
                       ],
                     ),
-                    Padding(
-                      padding: const EdgeInsets.all(20.0),
-                      child: Column(
-                        children: <Widget>[
-                          Text(
-                            "full name",
-                            style: TextStyle(fontSize: 18, letterSpacing: 0.4),
-                          ),
-                          Text(
-                            "Bio",
-                            style: TextStyle(fontSize: 16, letterSpacing: 0.4),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
             Container(
-              height: 165,
+              height: 120,
               width: MediaQuery.of(context).size.width,
               child: Card(
                 elevation: 8,
@@ -84,79 +76,83 @@ class _UserProfileState extends State<UserProfile> {
                     borderRadius: BorderRadius.circular(10),
                     borderSide: BorderSide(color: Colors.white)),
                 shadowColor: Colors.blue,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.only(top: 20.0),
-                      child: Column(
-                        children: [
-                          Text(
-                            "1.5",
-                            style: TextStyle(fontSize: 15),
-                          ),
-                          SizedBox(
-                            height: 6,
-                          ),
-                          Text(
-                            'Posts',
-                            style: TextStyle(
-                                fontSize: 15,
-                                letterSpacing: 0.4,
-                                fontWeight: FontWeight.bold),
-                          ),
-                        ],
+                child: Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.only(top: 20.0),
+                        child: Column(
+                          children: [
+                            Text(
+                              "1.5 k",
+                              style: TextStyle(fontSize: 15),
+                            ),
+                            SizedBox(
+                              height: 6,
+                            ),
+                            Text(
+                              'Posts',
+                              style: TextStyle(
+                                  fontSize: 15,
+                                  letterSpacing: 0.4,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                    SizedBox(width: 25),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 20.0),
-                      child: Column(
-                        children: [
-                          Text(
-                            "1.5 k",
-                            style: TextStyle(fontSize: 15),
-                          ),
-                          SizedBox(
-                            height: 6,
-                          ),
-                          Text(
-                            'Followers',
-                            style: TextStyle(
-                                fontSize: 15,
-                                letterSpacing: 0.4,
-                                fontWeight: FontWeight.bold),
-                          ),
-                        ],
+                      SizedBox(width: 25),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 20.0),
+                        child: Column(
+                          children: [
+                            Text(
+                              "1.5 k",
+                              style: TextStyle(fontSize: 15),
+                            ),
+                            SizedBox(
+                              height: 6,
+                            ),
+                            Text(
+                              'Followers',
+                              style: TextStyle(
+                                  fontSize: 15,
+                                  letterSpacing: 0.4,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                    SizedBox(
-                      width: 25,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 20.0),
-                      child: Column(
-                        children: [
-                          Text(
-                            "1.5 m",
-                            style: TextStyle(fontSize: 15, letterSpacing: 0.4),
-                          ),
-                          SizedBox(
-                            height: 6,
-                          ),
-                          Text(
-                            'Following',
-                            style: TextStyle(
-                                fontSize: 15,
-                                letterSpacing: 0.4,
-                                fontWeight: FontWeight.bold),
-                          ),
-                        ],
+                      SizedBox(
+                        width: 25,
                       ),
-                    ),
-                    Row()
-                  ],
+                      Padding(
+                        padding: const EdgeInsets.only(top: 20.0),
+                        child: Column(
+                          children: [
+                            Text(
+                              "1.5 m",
+                              style:
+                                  TextStyle(fontSize: 15, letterSpacing: 0.4),
+                            ),
+                            SizedBox(
+                              height: 6,
+                            ),
+                            Text(
+                              'Following',
+                              style: TextStyle(
+                                  fontSize: 15,
+                                  letterSpacing: 0.4,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Row(),
+                    ],
+                  ),
                 ),
               ),
             ),
