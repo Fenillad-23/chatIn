41ec010cb210ee9dbce39d719682e35ed27d183c
Fenillad-23 <75263786+Fenillad-23@users.noreply.github.com>
2022-01-27 14:12:21 +0530

    chatinPhotoGrid

--- lib/HomeScreen/UserProfile/UserProfile.dart
+++ lib/HomeScreen/UserProfile/UserProfile.dart
@@ -1,5 +1,7 @@
 import 'package:chattin/HomeScreen/User/UserPrefrences.dart';
 import 'package:flutter/material.dart';
+import 'package:transparent_image/transparent_image.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class UserProfile extends StatefulWidget {
   const UserProfile({Key? key}) : super(key: key);
@@ -25,188 +27,172 @@ class _UserProfileState extends State<UserProfile> {
           },
         ),
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(15.0),
-          child: ListView(
-            children: [
-              Container(
-                height: 155,
-                width: MediaQuery.of(context).size.width,
-                child: Row(
-                  children: <Widget>[
-                    Column(
-                      children: <Widget>[
-                        Padding(
-                          padding: const EdgeInsets.all(10.0),
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(100),
-                            child: Image.asset(
-                              'assets/images/user.jpeg',
-                              width: 125,
-                              height: 125,
-                              fit: BoxFit.contain,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Column(
+              children: [
+                Container(
+                  height: 155,
+                  width: MediaQuery.of(context).size.width,
+                  child: Row(
+                    children: <Widget>[
+                      Column(
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(100),
+                              child: Image.asset(
+                                'assets/images/user.jpeg',
+                                width: 125,
+                                height: 125,
+                                fit: BoxFit.contain,
+                              ),
                             ),
                           ),
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Column(
+                          children: <Widget>[
+                            Text(
+                              "full name",
+                              style:
+                                  TextStyle(fontSize: 18, letterSpacing: 0.4),
+                            ),
+                            Text(
+                              "Bio",
+                              style:
+                                  TextStyle(fontSize: 16, letterSpacing: 0.4),
+                            ),
+                          ],
                         ),
-                      ],
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  height: 120,
+                  width: MediaQuery.of(context).size.width,
+                  child: Card(
+                    elevation: 8,
+                    shape: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(color: Colors.white),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.all(20.0),
-                      child: Column(
+                    shadowColor: Colors.blue,
+                    child: Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
                         children: <Widget>[
-                          Text(
-                            "full name",
-                            style: TextStyle(fontSize: 18, letterSpacing: 0.4),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 20.0),
+                            child: Column(
+                              children: [
+                                Text(
+                                  "1.5 k",
+                                  style: TextStyle(fontSize: 15),
+                                ),
+                                SizedBox(
+                                  height: 6,
+                                ),
+                                Text(
+                                  'Posts',
+                                  style: TextStyle(
+                                      fontSize: 15,
+                                      letterSpacing: 0.4,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ],
+                            ),
                           ),
-                          Text(
-                            "Bio",
-                            style: TextStyle(fontSize: 16, letterSpacing: 0.4),
+                          SizedBox(width: 25),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 20.0),
+                            child: Column(
+                              children: [
+                                Text(
+                                  "1.5 k",
+                                  style: TextStyle(fontSize: 15),
+                                ),
+                                SizedBox(
+                                  height: 6,
+                                ),
+                                Text(
+                                  'Followers',
+                                  style: TextStyle(
+                                      fontSize: 15,
+                                      letterSpacing: 0.4,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ],
+                            ),
+                          ),
+                          SizedBox(
+                            width: 25,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 20.0),
+                            child: Column(
+                              children: [
+                                Text(
+                                  "1.5 m",
+                                  style: TextStyle(
+                                      fontSize: 15, letterSpacing: 0.4),
+                                ),
+                                SizedBox(
+                                  height: 6,
+                                ),
+                                Text(
+                                  'Following',
+                                  style: TextStyle(
+                                      fontSize: 15,
+                                      letterSpacing: 0.4,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ],
+                            ),
                           ),
                         ],
                       ),
                     ),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(
-                    bottom: 14.0, right: 10.0, top: 10.0, left: 10.0),
-                child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Container(
-                        height: 57,
-                        width: 92,
-                        //color: Colors.deepOrange,
-                        child: OutlinedButton.icon(
-                            onPressed: () {},
-                            style: OutlinedButton.styleFrom(
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(28.5),
-                                ),
-                                side: BorderSide(
-                                  width: 2.0,
-                                  color: Colors.blue,
-                                  style: BorderStyle.solid,
-                                )),
-                            icon: Container(
-                                padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
-                                child: Icon(Icons.chat_bubble_outline,
-                                    size: 28, color: Colors.black87)),
-                            label: Text("")),
-                      ),
-                      Container(
-                        height: 57,
-                        width: 208,
-                        //color: Colors.deepPurple,
-                        child: ElevatedButton(
-                          onPressed: () {
-                            // Respond to button press
-                          },
-                          style: ElevatedButton.styleFrom(
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(28.5),
-                            ),
-                          ),
-                          child: Text(
-                            'Follow',
-                            style: TextStyle(fontSize: 18),
-                          ),
-                        ),
-                      )
-                    ]),
-              ),
-              Container(
-                height: 120,
-                width: MediaQuery.of(context).size.width,
-                child: Card(
-                  elevation: 8,
-                  shape: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: BorderSide(color: Colors.white)),
-                  shadowColor: Colors.blue,
-                  child: Padding(
-                    padding: const EdgeInsets.all(15.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: <Widget>[
-                        Padding(
-                          padding: const EdgeInsets.only(top: 20.0),
-                          child: Column(
-                            children: [
-                              Text(
-                                "1.5 k",
-                                style: TextStyle(fontSize: 15),
-                              ),
-                              SizedBox(
-                                height: 6,
-                              ),
-                              Text(
-                                'Posts',
-                                style: TextStyle(
-                                    fontSize: 15,
-                                    letterSpacing: 0.4,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                            ],
-                          ),
-                        ),
-                        SizedBox(width: 25),
-                        Padding(
-                          padding: const EdgeInsets.only(top: 20.0),
-                          child: Column(
-                            children: [
-                              Text(
-                                "1.5 k",
-                                style: TextStyle(fontSize: 15),
-                              ),
-                              SizedBox(
-                                height: 6,
-                              ),
-                              Text(
-                                'Followers',
-                                style: TextStyle(
-                                    fontSize: 15,
-                                    letterSpacing: 0.4,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                            ],
-                          ),
-                        ),
-                        SizedBox(
-                          width: 25,
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(top: 20.0),
-                          child: Column(
-                            children: [
-                              Text(
-                                "1.5 m",
-                                style:
-                                    TextStyle(fontSize: 15, letterSpacing: 0.4),
-                              ),
-                              SizedBox(
-                                height: 6,
-                              ),
-                              Text(
-                                'Following',
-                                style: TextStyle(
-                                    fontSize: 15,
-                                    letterSpacing: 0.4,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                            ],
-                          ),
-                        ),
-                        Row(),
-                      ],
-                    ),
                   ),
                 ),
-              ),
-            ],
+                SizedBox(height: 30),
+                Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        height: MediaQuery.of(context).size.height,
+                        child: GridView(
+                          gridDelegate:
+                              SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 3,
+                          ),
+                          //  scrollDirection: Axis.horizontal,
+                          physics: BouncingScrollPhysics(),
+                          shrinkWrap: true,
+                          children: [
+                            Image.network('https://picsum.photos/250?image=1'),
+                            Image.network('https://picsum.photos/250?image=2'),
+                            Image.network('https://picsum.photos/250?image=3'),
+                            Image.network('https://picsum.photos/250?image=1'),
+                            Image.network('https://picsum.photos/250?image=2'),
+                            Image.network('https://picsum.photos/250?image=3'),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
