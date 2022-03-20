591e22cfd17f51e6b4997175697418f416ab8831
Fenillad-23 <75263786+Fenillad-23@users.noreply.github.com>
2022-03-20 14:25:50 +0530

    reflect

--- lib/HomeScreen/DashBoard.dart
+++ lib/HomeScreen/DashBoard.dart
@@ -1,5 +1,4 @@
 import 'package:chattin/HomeScreen/Storybar/StoryBarWidget.dart';
-import 'package:chattin/HomeScreen/UserProfileMain/UserProfileMain.dart';
 import 'package:chattin/Network/network_dio.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -28,209 +27,210 @@ class _DashboardState extends State<Dashboard> {
     SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
     dynamic response = await nw.httpPost(
         "user/homepage", {'username': sharedPreferences.getString("username")});
-    // print("Loading posts...${response['posts']}");
     if (response['statusCode'] != null ||
         response['statusCode'] == 200 && response['statusCode'] == '200') {
-      // dataList.add(response['posts'][0]);
       setState(() {});
-      // print("...................");
-
-      // print(response['posts'][0][0]['postedBy']);
-      for (int i = 0; i < response['posts'].length; i++) {
-        // print(response['posts'][i]);
-        // dataList.add(response['posts'][i]);
-      }
       dataList = response['posts'];
       loaded = true;
       setState(() {});
-      // print("print response posted by\n");
-      // print(response['posts'][0][0]['postedBy']);
-      // print("print posted by\n");
-      // print(dataList[0][0]['postedBy']);
-      print("print response image\n");
-      print(response['posts'][0][0]['image'][0]);
-      print("print image by\n");
-      print(dataList[0][0]['image'][0]);
     }
   }
 
   postWidget(BuildContext context) {
     return Column(
-        children: List.generate(dataList.length, (index) {
-      return Container(
-        // color: Colors.blue,
-        padding: EdgeInsets.only(bottom: 20),
-        child: Card(
-          elevation: 8,
-          shape: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(10),
-            borderSide: BorderSide(color: Colors.white),
-          ),
-          shadowColor: Color.fromRGBO(255, 255, 255, 1),
-          child: Container(
-            padding: EdgeInsets.all(15.0),
-            child: Column(
-              children: [
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Row(
-                      children: [
-                        CircleAvatar(
-                            radius: 20.0,
-                            child: ClipOval(
-                              child: Image.network(
-                                // dataList[index]['profilePic'].toString(),
-                                "https://images.unsplash.com/photo-1640622304233-7335e936f11b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxMXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
-                                height: 40,
-                                width: 40,
-                                fit: BoxFit.cover,
-                              ),
-                            )),
-                        Padding(
-                          padding: EdgeInsets.only(left: 10),
-                          child: Text(
-                            dataList[index][index]['postedBy'].toString(),
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 18),
-                          ),
-                        ),
-                      ],
+      children: List.generate(
+        dataList.length,
+        (index) {
+          return Column(
+            children: List.generate(
+              dataList[index].length,
+              (i) {
+                return Container(
+                  // color: Colors.blue,
+                  padding: EdgeInsets.only(bottom: 20),
+                  child: Card(
+                    elevation: 8,
+                    shape: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(color: Colors.white),
                     ),
-                    Container(
-                        height: 40,
-                        child: Padding(
-                          padding: EdgeInsets.only(top: 5),
-                          child: Icon(
-                            Icons.more_vert,
-                            size: 30,
+                    shadowColor: Color.fromRGBO(255, 255, 255, 1),
+                    child: Container(
+                      padding: EdgeInsets.all(15.0),
+                      child: Column(
+                        children: [
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Row(
+                                children: [
+                                  CircleAvatar(
+                                      radius: 20.0,
+                                      child: ClipOval(
+                                        child: Image.network(
+                                          // dataList[index]['profilePic'].toString(),
+                                          "https://images.unsplash.com/photo-1640622304233-7335e936f11b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxMXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+                                          height: 40,
+                                          width: 40,
+                                          fit: BoxFit.cover,
+                                        ),
+                                      )),
+                                  Padding(
+                                    padding: EdgeInsets.only(left: 10),
+                                    child: Text(
+                                      dataList[index][i]['postedBy'].toString(),
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 18),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Container(
+                                  height: 40,
+                                  child: Padding(
+                                    padding: EdgeInsets.only(top: 5),
+                                    child: Icon(
+                                      Icons.more_vert,
+                                      size: 30,
+                                    ),
+                                  )),
+                            ],
                           ),
-                        )),
-                  ],
-                ),
-                SizedBox(
-                  height: 5,
-                ),
-                Divider(),
-                SizedBox(
-                  height: 7,
-                ),
-                Row(
-                  children: [
-                    Container(
-                      width: MediaQuery.of(context).size.width - 58,
-                      child: Text(
-                        dataList[index][index]['caption'].toString(),
-                        style: TextStyle(
-                            // color: Colors.black.withOpacity(0.6),
-                            fontSize: 12),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Divider(),
+                          SizedBox(
+                            height: 7,
+                          ),
+                          Row(
+                            children: [
+                              Container(
+                                width: MediaQuery.of(context).size.width - 58,
+                                child: Text(
+                                  dataList[index][i]['caption'].toString(),
+                                  style: TextStyle(
+                                      // color: Colors.black.withOpacity(0.6),
+                                      fontSize: 12),
+                                ),
+                              )
+                            ],
+                          ),
+                          SizedBox(
+                            height: 12,
+                          ),
+                          Row(
+                            children: [
+                              Container(
+                                width: MediaQuery.of(context).size.width - 58,
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(10.0),
+                                  child: Image.network(
+                                    dataList[index][i]['image'][0].toString(),
+                                    // "https://images.unsplash.com/photo-1640622304233-7335e936f11b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxMXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Row(
+                                children: [
+                                  GestureDetector(
+                                      // child: dataList[index]['isLike'] == true
+                                      //     ? Icon(
+                                      //         Icons.favorite,
+                                      //         color: Colors.red,
+                                      //         size: 32,
+                                      //       )
+                                      //     : Icon(
+                                      //         Icons.favorite_outline,
+                                      //         size: 32,
+                                      //       ),
+                                      child: Icon(
+                                        Icons.favorite_outline,
+                                        size: 32,
+                                      ),
+                                      onTap: () {
+                                        // dataList[index]['isLike'] =
+                                        //     !dataList[index]['isLike'];
+                                        // setState(() {});
+                                      }),
+                                  SizedBox(
+                                    width: 25,
+                                  ),
+                                  Icon(FontAwesomeIcons.comment),
+                                  SizedBox(
+                                    width: 25,
+                                  ),
+                                  new Icon(FontAwesomeIcons.paperPlane),
+                                  // const Text("45",style: TextStyle(fontSize: 15),),
+                                ],
+                              ),
+                              InkWell(
+                                onTap: () {
+                                  // dataList[index]['isSaved'] =
+                                  //     !dataList[index]['isSaved'];
+                                  // setState(() {});
+                                },
+                                child: Icon(
+                                  Icons.bookmark,
+                                  // dataList[index]['isSaved'] == true
+                                  //     ? Icons.bookmark
+                                  //     : Icons.bookmark_outline,
+                                  size: 32,
+                                ),
+                              )
+                            ],
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Row(
+                            children: [
+                              Text(
+                                '0', //dataList[index]['likesCount'],
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 14,
+                                ),
+                              )
+                            ],
+                          ),
+                          SizedBox(
+                            height: 3,
+                          ),
+                          Row(
+                            children: [
+                              Text(
+                                '1 hour ago',
+                                // dataList[index]['duration'],
+                                style: TextStyle(
+                                  color: Colors.black.withOpacity(0.6),
+                                  fontSize: 12,
+                                ),
+                              )
+                            ],
+                          ),
+                        ],
                       ),
-                    )
-                  ],
-                ),
-                SizedBox(
-                  height: 12,
-                ),
-                Row(
-                  children: [
-                    Container(
-                      width: MediaQuery.of(context).size.width - 58,
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(10.0),
-                        child: Image.network(
-                          dataList[index][index]['image'][0].toString(),
-                          // "https://images.unsplash.com/photo-1640622304233-7335e936f11b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxMXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                // Row(
-                //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                //   children: [
-                //     Row(
-                //       children: [
-                //         GestureDetector(
-                //             child: dataList[index]['isLike'] == true
-                //                 ? Icon(
-                //                     Icons.favorite,
-                //                     color: Colors.red,
-                //                     size: 32,
-                //                   )
-                //                 : Icon(
-                //                     Icons.favorite_outline,
-                //                     size: 32,
-                //                   ),
-                //             onTap: () {
-                //               dataList[index]['isLike'] =
-                //                   !dataList[index]['isLike'];
-                //               setState(() {});
-                //             }),
-                //         SizedBox(
-                //           width: 25,
-                //         ),
-                //         Icon(FontAwesomeIcons.comment),
-                //         SizedBox(
-                //           width: 25,
-                //         ),
-                //         new Icon(FontAwesomeIcons.paperPlane),
-                //         // const Text("45",style: TextStyle(fontSize: 15),),
-                //       ],
-                //     ),
-                //     InkWell(
-                //       onTap: () {
-                //         dataList[index]['isSaved'] =
-                //             !dataList[index]['isSaved'];
-                //         setState(() {});
-                //       },
-                //       child: Icon(
-                //         dataList[index]['isSaved'] == true
-                //             ? Icons.bookmark
-                //             : Icons.bookmark_outline,
-                //         size: 32,
-                //       ),
-                //     )
-                //   ],
-                // ),
-                SizedBox(
-                  height: 10,
-                ),
-                // Row(
-                //   children: [
-                //     Text(
-                //       dataList[index]['likesCount'],
-                //       style: TextStyle(
-                //         fontWeight: FontWeight.bold,
-                //         fontSize: 14,
-                //       ),
-                //     )
-                //   ],
-                // ),
-                SizedBox(
-                  height: 3,
-                ),
-                // Row(
-                //   children: [
-                //     Text(
-                //       dataList[index]['duration'],
-                //       style: TextStyle(
-                //         color: Colors.black.withOpacity(0.6),
-                //         fontSize: 12,
-                //       ),
-                //     )
-                //   ],
-                // ),
-              ],
+                    ),
+                  ),
+                );
+              },
             ),
-          ),
-        ),
-      );
-    }));
+          );
+        },
+      ),
+    );
   }
 
   @override
